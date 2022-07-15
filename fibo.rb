2d4b57959d510cf34dd56eb7e413fc99a445a2a0
JCPedroza <nemesis4go10@hotmail.com>
2022-07-15 03:53:08 -0500

    feat: add variant

--- fibo.rb
+++ fibo.rb
@@ -1,9 +1,25 @@
+# frozen_string_literal: true
+
 # Run me with ruby fibo.rb
 
 # Compute nth Fibonacci number using simple recursion.
-def fibo_simple index
+def fibo_simple(index)
   return index if index < 2
-  return fibo_simple(index - 1) + fibo_simple(index - 2)
+
+  fibo_simple(index - 1) + fibo_simple(index - 2)
+end
+
+# Compute nth Fibonacci number using a times loop.
+def fibo_times(index)
+  now = 0
+  nxt = 1
+
+  index.times do |_|
+    now, nxt = nxt, now + nxt
+  end
+
+  now
 end
 
 puts fibo_simple 6
+puts fibo_times 6
