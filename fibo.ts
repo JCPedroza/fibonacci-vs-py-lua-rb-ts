f65badb4db992fff98d0ca2497a37ec17a1409ed
JCPedroza <nemesis4go10@hotmail.com>
2022-07-24 06:27:51 -0500

    add simple unit testing

--- fibo.ts
+++ fibo.ts
@@ -5,12 +5,23 @@ function fiboSimple(index: number): number {
   return fiboSimple(index - 1) + fiboSimple(index - 2)
 }
 
+// Compute nth Fibonacci number using simpre recursion and a switch.
+function fiboSimpleSwitch(index: number): number {
+  switch (index < 2) {
+    case true:
+      return index
+    default:
+      return fiboSimpleSwitch(index - 1) + fiboSimpleSwitch(index - 2)
+  }
+}
+
+
 // Compute nth Fibonacci number using tail call.
 function fiboTailCall(index: number ): number {
-  function loop(now: number, nxt: number, index: number): number {
+  function loop(now: number, next: number, index: number): number {
     if (index === 0) return now
 
-    return loop(nxt, now + nxt, index - 1)
+    return loop(next, now + next, index - 1)
   }
 
   return loop(0, 1, index)
@@ -60,12 +71,80 @@ function fiboMemoizedMap(index: number): number {
   return loop(index)
 }
 
+// Compute nth Fibonacci number using a for loop.
+function fiboForLoop(index: number): number {
+  let now = 0
+  let next = 1
+
+  for (let loop = 0; loop < index; loop++) {
+    [now, next] = [next, now + next]
+  }
+
+  return now
+}
+
+// Compute nth Fibonacci number using a while loop.
+function fiboWhileLoop(index: number): number {
+  let now = 0
+  let next = 1
+  let loop = 0
+
+  while (loop < index) {
+    [now, next] = [next, now + next]
+    loop++
+  }
+
+  return now
+}
+
+// Algorithms to unit test and profile.
 const fibosToTest: Function[] = [
   fiboSimple,
+  fiboSimpleSwitch,
   fiboTailCall,
   fiboMemoizedArray,
   fiboMemoizedObject,
-  fiboMemoizedMap
+  fiboMemoizedMap,
+  fiboForLoop,
+  fiboWhileLoop
 ]
 
-fibosToTest.forEach(fibo => console.log(`${fibo.name}: ${fibo(6)}`))
+// Testing
+
+// Check that a function evaluates to the expected output.
+const assertEquals = (fun: Function, arg: number, out: number) => {
+  const result = fun(arg)
+  const passed = result === out
+
+  if (!passed) {
+    throw new Error(`${fun.name} failed unit test f(${arg}) === ${out} with ${result}`)
+  }
+}
+
+// Run all unit test assertions.
+const assertAll = (fun: Function) => {
+  assertEquals(fun, 0, 0)
+  assertEquals(fun, 1, 1)
+  assertEquals(fun, 2, 1)
+  assertEquals(fun, 3, 2)
+  assertEquals(fun, 4, 3)
+  assertEquals(fun, 5, 5)
+  assertEquals(fun, 6, 8)
+  assertEquals(fun, 30, 832_040)
+}
+
+// Perform unit tests for one algorithm.
+const testFunction = (fun: Function, verbose: boolean = true) => {
+  if (verbose) {
+    console.log(`Testing algorithm ${fun.name}...`)
+  }
+
+  assertAll(fun)
+}
+
+// Run unit tests for all functions.
+const testAllFunctions = (funs: Function[]) => {
+  funs.forEach(fun => testFunction(fun))
+}
+
+testAllFunctions(fibosToTest)
