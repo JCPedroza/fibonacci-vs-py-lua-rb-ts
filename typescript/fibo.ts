6574a1bfef2c17def90ef072110d8d7285d9cb1f
JCPedroza <nemesis4go10@hotmail.com>
2022-07-26 11:03:33 -0500

    refa: implement tests in a separate module

--- typescript/fibo.ts
+++ typescript/fibo.ts
@@ -8,6 +8,9 @@
  * large values.
  */
 
+import { testAllFunctions } from './test'
+
+
 // Compute nth Fibonacci number using simple recursion.
 function fiboSimpleIf(index: number): number {
   if (index < 2) return index
@@ -151,40 +154,5 @@ const fibosToTest: Function[] = [
   fiboBinet,
   fiboGenerator
 ]
-
-// Testing
-
-// Check that a function evaluates to the expected output.
-const assertEquals = (fun: Function, arg: number, out: number) => {
-  const result = fun(arg)
-  const passed = result === out
-
-  if (!passed) {
-    throw new Error(`${fun.name} failed unit test f(${arg}) === ${out} with ${result}`)
-  }
-}
-
-// Run all unit test assertions.
-const assertAll = (fun: Function) => {
-  assertEquals(fun, 0, 0)
-  assertEquals(fun, 1, 1)
-  assertEquals(fun, 2, 1)
-  assertEquals(fun, 3, 2)
-  assertEquals(fun, 4, 3)
-  assertEquals(fun, 5, 5)
-  assertEquals(fun, 6, 8)
-  assertEquals(fun, 30, 832_040)
-}
-
-// Perform unit tests for one algorithm.
-const testFunction = (fun: Function) => {
-  console.log(`Testing algorithm ${fun.name}...`)
-  assertAll(fun)
-}
-
-// Run unit tests for all functions.
-const testAllFunctions = (funs: Function[]) => {
-  funs.forEach(fun => testFunction(fun))
-}
 
 testAllFunctions(fibosToTest)
---
+++ typescript/test.ts
@@ -0,0 +1,36 @@
+// Check that a function evaluates to the expected output.
+const assertEquals = (fun: Function, arg: number, out: number) => {
+  const result = fun(arg)
+  const passed = result === out
+
+  if (!passed) {
+    throw new Error(`${fun.name} failed unit test f(${arg}) === ${out} with ${result}`)
+  }
+}
+
+// Run all unit test assertions.
+const assertAll = (fun: Function) => {
+  assertEquals(fun, 0, 0)
+  assertEquals(fun, 1, 1)
+  assertEquals(fun, 2, 1)
+  assertEquals(fun, 3, 2)
+  assertEquals(fun, 4, 3)
+  assertEquals(fun, 5, 5)
+  assertEquals(fun, 6, 8)
+  assertEquals(fun, 30, 832_040)
+}
+
+// Perform unit tests for one algorithm.
+const testFunction = (fun: Function) => {
+  console.log(`Testing algorithm ${fun.name}...`)
+  assertAll(fun)
+}
+
+// Run unit tests for all functions.
+export const testAllFunctions = (funs: Function[]) => {
+  funs.forEach(fun => testFunction(fun))
+}
+
+export default {
+  testAllFunctions
+}
