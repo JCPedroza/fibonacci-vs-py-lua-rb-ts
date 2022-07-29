6063a9c262472ba9d2785460ac579769467a5c57
JCPedroza <nemesis4go10@hotmail.com>
2022-07-29 15:12:36 -0500

    feat: ask for range parameters to user in cli

--- python/main.py
+++ python/main.py
@@ -10,9 +10,7 @@ from profile import profile_all, profile_all_range
 
 if __name__ == "__main__":
     cli_args = parse_args()
-    index, reps = ask_params(cli_args)
+    index, reps, range_start, range_end, range_reps = ask_params(cli_args)
     test_all(fibos)
     profile_all(fibos, index, reps)
-    profile_all_range(fibos, 3, 27, 10)
-
-# TODO range profile parameters from cli
+    profile_all_range(fibos, range_start, range_end, range_reps)
